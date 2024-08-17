a17303c5c368e0bb46fe72eca6978a90303e019d
Midori Takahashi <mintgreen7programming@gmail.com>
2024-08-18 03:14:28 +0900

    ADD:デザイン部分を作成, TODO:リンクの修正、APIを機能させる

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import full_screenshot from './assets/full_screenshot.svg'
+import crop from './assets/crop.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
+      <div className="logo-container">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={full_screenshot} className="logo" alt="full_screenshot logo" />
+          <div>Full ScreenShot</div>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={crop} className="logo" alt="crop logo" />
+          <div>Select & Capture</div>
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="read-the-docs">
+        <p >
+          Your support helps us improve and maintain.
         </p>
+        <div>
+          <a href="https://react.dev" target="_blank">Give Feedback</a>
+          <div> and </div>
+          <a href="https://react.dev" target="_blank">Support Us</a>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
