ba5edf25851e5843e6c160afdaa8660dbad0e6d5
Midori Takahashi <mintgreen7programming@gmail.com>
2024-08-23 05:14:02 +0900

    FIX:コンポーネントを適用, TODO:imgではなくsvgに

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import Button from './compenents/Button'
+import SupportSection from './compenents/SupportSection'
 import full_screenshot from './assets/full_screenshot.svg'
+import { takeFullScreenshot } from './popup'
 import crop from './assets/crop.svg'
 import './App.css'
 
@@ -7,25 +9,20 @@ function App() {
   return (
     <>
       <div className="logo-container">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={full_screenshot} className="logo" alt="full_screenshot logo" />
-          <div>Full ScreenShot</div>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={crop} className="logo" alt="crop logo" />
-          <div>Select & Capture</div>
-        </a>
+        <Button
+          onClick={takeFullScreenshot}
+          imgSrc={full_screenshot}
+          imgAlt="full_screenshot logo"
+          text="Full ScreenShot"
+        />
+        <Button
+          onClick={takeFullScreenshot}
+          imgSrc={crop}
+          imgAlt="crop logo"
+          text="Select & Capture"
+        />
       </div>
-      <div className="read-the-docs">
-        <p >
-          Your support helps us improve and maintain.
-        </p>
-        <div>
-          <a href="https://react.dev" target="_blank">Give Feedback</a>
-          <div> and </div>
-          <a href="https://react.dev" target="_blank">Support Us</a>
-        </div>
-      </div>
+      <SupportSection />
     </>
   )
 }
