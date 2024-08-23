000fca12dbc0f57e994f2cb90f945be389b52702
Midori Takahashi <mintgreen7programming@gmail.com>
2024-08-24 04:42:37 +0900

    bun run build

---
+++ src/compenents/SupportSection.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+function SupportSection() {
+    return (_jsxs("div", { className: "read-the-docs", children: [_jsx("p", { children: "Your support helps us improve and maintain." }), _jsxs("div", { children: [_jsx("a", { href: "https://react.dev", target: "_blank", rel: "noopener noreferrer", children: "Give Feedback" }), _jsx("div", { children: " and " }), _jsx("a", { href: "https://www.amazon.jp/hz/wishlist/ls/276Y4QG70S7G6?ref_=wl_share", target: "_blank", rel: "Support", children: "Support Us" })] })] }));
+}
+export default SupportSection;
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), tsconfigPaths(), crx({ manifest: manifest })],
+    server: {
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            port: 5173,
+        },
+    },
+});
