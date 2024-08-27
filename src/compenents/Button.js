f1042da5acd16564ebe98f868fa4b61cbd8f4b76
Midori Takahashi <mintgreen7programming@gmail.com>
2024-08-28 03:34:45 +0900

    ビルドファイルの出力先を変更

--- src/compenents/Button.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-function Button(_a) {
-    var onClick = _a.onClick, imgSrc = _a.imgSrc, imgAlt = _a.imgAlt, text = _a.text;
-    return (_jsxs("button", { onClick: onClick, className: "api-button", children: [_jsx("img", { src: imgSrc, className: "logo", alt: imgAlt }), _jsx("div", { children: text })] }));
-}
-export default Button;
