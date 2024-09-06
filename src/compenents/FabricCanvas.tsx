26996522c1c51ba896acea5c45addce80e18c7e1
Midori Takahashi <mintgreen7programming@gmail.com>
2024-09-06 21:39:26 +0900

    postMessage APIで画像情報をやり取りさせる

---
+++ src/compenents/FabricCanvas.tsx
@@ -0,0 +1,114 @@
+import { useEffect, useRef } from 'react';
+import { Canvas, FabricImage, Textbox, Rect, Pattern, Circle } from 'fabric';
+
+import mosaicPatternImg from '../assets/mosaicPattern.png';
+
+export const FabricCanvas = ({ screenshotUrl, addTextRef, addMosaicRef, addShapeRef }) => {
+  const canvasEl = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<Canvas | null>(null);
+
+  // キャンバスの初期化
+  useEffect(() => {
+    if (!canvasEl.current) return;
+
+    // キャンバスの設定
+    const options = {
+      width: 1000,
+      height: 600,
+      selection: false, // 選択の無効化
+    }
+    const canvas = new Canvas(canvasEl.current, options);
+    canvasRef.current = canvas;
+
+    FabricImage.fromURL(screenshotUrl).then((img) => {
+      canvas.add(img); // 画像をキャンバスに追加
+      canvas.renderAll(); // キャンバスを再描画
+    }).catch((error) => {
+      console.error('Error loading image:', error);
+    });
+
+    return () => {
+      canvas.dispose(); // クリーンアップ
+    };
+  }, [screenshotUrl]);
+
+  // TextBox
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    addTextRef.current = () => {
+      const textbox = new Textbox('Enter text here', {
+        left: 100,
+        top: 100,
+        width: 200,
+        fontSize: 20,
+        fill: '#000000',
+      });
+
+      canvasRef.current?.add(textbox);
+      canvasRef.current?.setActiveObject(textbox);
+      canvasRef.current?.renderAll();
+    }
+  }, [addTextRef]);
+
+  // モザイク
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    addMosaicRef.current = () => {
+      FabricImage.fromURL(mosaicPatternImg).then((img) => {
+        const pattern = new Pattern({
+          source: img._element,
+          repeat: 'repeat',
+        });
+
+        const mosaicPattern = new Rect({
+          left: 100,
+          top: 100,
+          width: 200,
+          height: 40,
+          fill: pattern
+        });
+        canvasRef.current?.add(mosaicPattern);
+        canvasRef.current?.renderAll();
+      }).catch((error) => {
+        console.error('Error loading mosaic pattern image:', error);
+      });
+    }
+  }, [addMosaicRef]);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    addShapeRef.current = (shape: 'rectangle' | 'circle') => {
+      let shapeObj;
+      if (shape === 'rectangle') {
+        shapeObj = new Rect({
+          left: 100,
+          top: 100,
+          width: 200,
+          height: 40,
+          fill: 'transparent',
+          stroke: 'red',
+          strokeWidth: 2,
+        });
+      } else if (shape === 'circle') {
+        shapeObj = new Circle({
+          left: 150,
+          top: 150,
+          radius: 50,
+          fill: 'green'
+        });
+      }
+
+      if (shapeObj) {
+        canvasRef.current?.add(shapeObj);
+        canvasRef.current?.renderAll();
+      }
+    }
+  }, [addShapeRef]);
+
+  return <canvas ref={canvasEl}/>;
+}
+
+export default FabricCanvas;
